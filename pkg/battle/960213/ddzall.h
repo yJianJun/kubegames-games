ed5bebd80c5b4a4143459bff9eedb6d99078877d
yejj5 <yejj5@lenovo.com>
2024-08-25 12:45:20 +0800

    初始化

--- pkg/battle/960213/ddzall.h
+++
@@ -1,5 +0,0 @@
-//ddzall.h
-#ifndef DDZALL_H
-#define DDZALL_H
-char* get_action_plus(char* state, int player, int playnum); // 声明
-#endif
---
+++ pkg/battle/landlord/game/game_test.go
@@ -0,0 +1,38 @@
+package game
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/kubegames/kubegames-games/pkg/battle/landlord/msg"
+	"github.com/kubegames/kubegames-sdk/pkg/log"
+)
+
+func TestDouDizhu_GameStart(t *testing.T) {
+	putCardsReq := &msg.PutCardsReq{
+		Cards: []byte{51},
+	}
+	buffer, err := proto.Marshal(putCardsReq)
+	if err != nil {
+		log.Errorf("proto marshal fail : %v", err.Error())
+		return
+	}
+
+	req := &msg.PutCardsReq{}
+	if err := proto.Unmarshal(buffer, req); err != nil {
+		log.Errorf("解析出牌入参错误: %v", err.Error())
+		return
+	}
+
+	t.Log(putCardsReq.Cards[0])
+	t.Log(req.Cards[0])
+
+	//t.Log(putCardsReq.Cards[0] == req.Cards[0])
+}
+
+func TestRobot_Init(t *testing.T) {
+	var a [][]int
+	a = append(a, []int{})
+	t.Log(a)
+	t.Log(len(a))
+}
